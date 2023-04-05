d8f618f353d67cf572191c5bfc7020e6ffa5ccf1
Deeti <deetigupta8@gmail.com>
2023-04-05 17:40:41 +0530

    getting familiar with antdesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { ProTable } from '@ant-design/pro-components';
+
 
 
 function App() {
+  const todos = [{
+    title: "Read",
+    description: "read a book"
+  },
+  {
+    title: "Write",
+    description: "write a blog"
+  }
+  ]
   return (
-    <h1>Todo Table</h1>
+    <>
+      <h1>Todo Table</h1>
+      <ProTable
+        toolBarRender={false}
+        search={false}
+        dataSource={todos}
+        pagination={false}
+
+        columns={[
+          { title: "Time Stamp" },
+          { title: "Title", dataIndex: "title" },
+          { title: "Description", dataIndex: "description" },
+          { title: "Due Date" },
+          { title: "Status" },
+          { title: "Tag" },
+          { title: "Modify" },
+          { title: "Delete" }
+        ]}>
+      </ProTable>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { ConfigProvider } from 'antd';
+import enUS from 'antd/locale/en_US';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ConfigProvider locale={enUS}>
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
 )
