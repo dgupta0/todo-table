be93c8e794b330329359a46c96a0d043ed30f1a8
Deeti <deetigupta8@gmail.com>
2023-04-06 23:42:46 +0530

    render basic todo task in table done

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,52 @@ import React from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { ProTable } from '@ant-design/pro-components';
-import { Input } from 'antd';
+import { Button } from 'antd';
 
 
 
 
 function App() {
   const [isForm, setIsForm] = React.useState(false)
-  const [task, setTask] = React.useState([])
+  const [task, setTask] = React.useState({})
+  const [todos, setToDos] = React.useState([
+    {
+      title: "Read",
+      description: "read a book"
+    },
+    {
+      title: "Write",
+      description: "write a blog",
+      tag: ["marketing", "finance"]
+    }
+  ])
 
   function setUpTask() {
     setIsForm(prev => !prev)
+  }
 
+  function handleChange(e) {
+    console.log(task)
+    console.log(e.target, e.target.value)
+    setTask(prev => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      }
+    })
   }
   function saveDetails() {
-    console.log("click")
+    setToDos(prev => {
+      let newArr = [...prev]
+      newArr.push(task)
+      console.log(newArr)
+      return newArr
+    })
+    setTask({})
+    setIsForm(false)
+
   }
-  const todos = [{
-    title: "Read",
-    description: "read a book"
-  },
-  {
-    title: "Write",
-    description: "write a blog"
-  }
-  ]
+
   return (
     <>
       <h1>Todo Table</h1>
@@ -45,55 +66,148 @@ function App() {
         pagination={false}
 
         columns={[
-          { title: "Time Stamp" },
+          { title: "Time Stamp", dataIndex: "timeStamp" },
           { title: "Title", dataIndex: "title" },
           { title: "Description", dataIndex: "description" },
-          { title: "Due Date" },
-          { title: "Status" },
-          { title: "Tag" },
-          { title: "Modify" },
-          { title: "Delete" }
+          { title: "Due Date", dataIndex: "duedate" },
+          { title: "Status", dataIndex: "status" },
+          { title: "Tag", dataIndex: "tag" },
+          {
+            title: "Modify",
+            render: () => [
+              <button className='formBtn edit'>Edit</button>
+            ]
+          },
+          {
+            title: "Delete",
+            render: () => [
+              <button className='formBtn del'>Delete</button>
+            ]
+          }
         ]}>
       </ProTable>
-      {isForm &&
+      {
+        isForm &&
         <div className='formModal'>
           <form>
-            <label for="TimeStamp">Timestamp:</label>
-            <input type="date" id="TimeStamp" name="TimeStamp" />
-            <label for="title">Todo Title:</label>
-            <input type="name" id="title" name="title" maxlength="100" /> <br />
-            <label for="desc">Task Description:</label>
-            <input type="name" id="desc" name="desc" maxlength="100" />
-            <label for="duedate">Due Date:</label>
-            <input type="date" id="duedate" name="duedate" />
+            <div className='pairInput'>
+              <label htmlFor="TimeStamp">Timestamp:
+                <input
+                  onChange={handleChange}
+                  type="date"
+                  id="TimeStamp"
+                  name="timeStamp"
+                />
+              </label>
+
+              <label htmlFor="title">Title:
+                <input
+                  onChange={handleChange}
+                  type="name"
+                  id="title"
+                  name="title"
+                  maxLength="100"
+                />
+              </label>
+
+            </div>
+            <div className='pairInput'>
+              <label htmlFor="desc">Description:
+                <input
+                  onChange={handleChange}
+                  type="name"
+                  id="desc"
+                  name="description"
+                  maxLength="100"
+                />
+              </label>
+              <label htmlFor="duedate">Due Date:
+                <input
+                  onChange={handleChange}
+                  type="date"
+                  id="dueDate"
+                  name="duedate"
+                />
+              </label>
+            </div>
             <fieldset>
               <legend>Task domain(s):</legend>
-              <input type="checkbox" id="ops" name="ops" />
-              <label for="ops">Operation</label> <br />
-              <input type="checkbox" id="finance" name="finance" />
-              <label for="finance">Finance</label> <br />
-              <input type="checkbox" id="marketing" name="marketing" />
-              <label for="marketing">Marketing</label> <br />
-              <input type="checkbox" id="tech" name="tech" />
-              <label for="tech">Tech</label>
+              <input
+                onChange={handleChange}
+                type="checkbox"
+                id="ops"
+                value="Operation"
+                name="tags"
+              />
+              <label htmlFor="ops">Operation</label> <br />
+              <input
+                onChange={handleChange}
+                type="checkbox"
+                id="finance"
+                value="Finace"
+                name="tags"
+              />
+              <label htmlFor="finance">Finance</label> <br />
+              <input
+                onChange={handleChange}
+                type="checkbox"
+                id="marketing"
+                value="Marketing"
+                name="tags"
+              />
+              <label htmlFor="marketing">Marketing</label> <br />
+              <input
+                onChange={handleChange}
+                type="checkbox"
+                id="tech"
+                value="Tech"
+                name="tags"
+              />
+              <label htmlForr="tech">Tech</label>
             </fieldset>
+
             <fieldset required>
               <legend> Task status:</legend>
-              <input type="radio" id="open" name="open" />
-              <label for="open">Open</label> <br />
-              <input type="radio" id="working" name="working" />
-              <label for="working">Working</label> <br />
-              <input type="radio" id="done" name="done" />
-              <label for="done">Done</label> <br />
-              <input type="radio" id="due" name="due" />
-              <label for="due">Overdue</label>
+              <input
+                onChange={handleChange}
+                type="radio"
+                id="open"
+                value="Open"
+                name="status"
+              />
+              <label htmlFor="open">Open</label> <br />
+              <input
+                onChange={handleChange}
+                type="radio"
+                id="working"
+                value="Working"
+                name="status"
+              />
+              <label htmlFor="working">Working</label> <br />
+              <input
+                onChange={handleChange}
+                type="radio"
+                id="done"
+                value="Done"
+                name="status"
+              />
+              <label htmlFor="done">Done</label> <br />
+              <input
+                onChange={handleChange}
+                type="radio"
+                id="due"
+                value="Overdue"
+                name="status"
+              />
+              <label htmlFor="due">Overdue</label>
             </fieldset>
-            <button id='submit' type="button" onClick={saveDetails}>Save</button>
-            <button id="cancel" type="button" onClick={setUpTask}>Cancel</button>
+            <div className='btnContainer'>
+              <button id='submit' type="button" onClick={saveDetails}>Save</button>
+              <button id="cancel" type="button" onClick={setUpTask}>Cancel</button>
+            </div>
           </form>
         </div>
       }
-
     </>
   )
 }
